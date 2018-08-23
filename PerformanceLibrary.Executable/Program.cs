18f5473689857d6aa3926eae1934560a2b3e4f59
Cliff Chapman <cliff@NebbiaTech.com>
2018-08-23 13:26:11 -0400

    Add simple performance relationship project

---
+++ PerformanceLibrary.Executable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PerformanceLibrary.Executable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running StringBuilder");
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            PerformanceLibrary.Class1.TightLoopStringBuilder();
+            watch.Stop();
+            Console.WriteLine($"Time Elapsed: {watch.ElapsedMilliseconds}ms");
+
+            Console.WriteLine("Running Concatenate");
+            watch.Reset();
+            watch.Start();
+            PerformanceLibrary.Class1.TightLoopConcatenate();
+            watch.Stop();
+            Console.WriteLine($"Tme Elapsed: {watch.ElapsedMilliseconds}ms");
+            Console.WriteLine("Press enter to close the app.");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ PerformanceLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PerformanceLibrary.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void TestConcatenate()
+        {
+            var result = PerformanceLibrary.Class1.TightLoopConcatenate();
+
+            TestContext.WriteLine($"Resulting text length is {result.Length}");
+            Assert.AreEqual(200000, result.Length);
+        }
+
+        [TestMethod]
+        public void TestStringBuilder()
+        {
+            var result = PerformanceLibrary.Class1.TightLoopStringBuilder();
+
+            TestContext.WriteLine($"Resulting text length is {result.Length}");
+            Assert.AreEqual(200000, result.Length);
+        }
+    }
+}
