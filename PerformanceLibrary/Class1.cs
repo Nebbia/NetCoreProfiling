3b37f76c00ae8032fa090e30f7313907379e264d
Cliff Chapman <cliff@NebbiaTech.com>
2018-08-23 11:52:50 -0400

    Add project skeleton

--- PerformanceLibrary/Class1.cs
+++ PerformanceLibrary/Class1.cs
@@ -1,8 +1,33 @@
-﻿using System;
+﻿using System.Text;
 
 namespace PerformanceLibrary
 {
-    public class Class1
+    public static class Class1
     {
+        public static string TightLoopConcatenate()
+        {
+            var source = "test";
+            var result = string.Empty;
+
+            for (var i = 0; i < 50000; i++)
+            {
+                result += source;
+            }
+
+            return result;
+        }
+
+        public static string TightLoopStringBuilder()
+        {
+            var source = "test";
+            var sb = new StringBuilder();
+
+            for (var i = 0; i < 50000; i++)
+            {
+                sb.Append(source);
+            }
+
+            return sb.ToString();
+        }
     }
 }
